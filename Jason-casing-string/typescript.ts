f9e5e4467f53f335d1b27a1dae88795fe121846b
AlexGrohmann <alex.grohmann@googlemail.com>
2023-06-26 21:56:32 +0200

    typescript

---
+++ Jason-casing-string/typescript.ts
@@ -0,0 +1,13 @@
+String.prototype.toJadenCase = function () {
+  return this.split(" ")
+    .map((w) => w[0].toUpperCase() + w.slice(1))
+    .join(" ");
+};
+
+var str = "How can mirrors be real if our eyes aren't real";
+console.log(str.toJadenCase());
+
+interface String {
+  // Declaration needed, don't remove it
+  toJadenCase(): string;
+}
