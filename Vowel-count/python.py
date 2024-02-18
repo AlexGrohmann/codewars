0034e8d1735ee432589debc88107ec7f60c619d7
AlexGrohmann <alex.grohmann@googlemail.com>
2024-02-18 20:34:40 +0100

    vowel count python

---
+++ Vowel-count/python.py
@@ -0,0 +1,6 @@
+def get_count(sentence):
+    count = 0
+    for char in sentence:
+        if char == "a" or char == "e" or char == "i" or char == "o" or char == "u":
+            count = count + 1
+    return count
