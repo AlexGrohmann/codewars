9a991405d76fe2c40a802280280780116b5bd71e
AlexGrohmann <alex.grohmann@googlemail.com>
2024-02-19 21:18:13 +0100

    stop spinning my words python

---
+++ Stop-spinning-my-words/python.py
@@ -0,0 +1,9 @@
+def spin_words(sentence):
+    words = sentence.split()
+    result = ""
+    for word in words:
+        if len(word) >= 5:
+            result += " " + word[::-1]
+        else:
+            result += " " + word
+    return result.strip()
