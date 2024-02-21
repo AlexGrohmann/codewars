7e91b15269a0eb01fa4f3dcccdf8e466cee43d57
AlexGrohmann <alex.grohmann@googlemail.com>
2024-02-21 20:47:08 +0100

    Highest and lowest python

--- Highest-and-lowest/python.py
+++ Highest-and-lowest/python.py
@@ -1,14 +1,8 @@
 def high_and_low(numbers):
-    split = numbers.split(" ")
-    maxValue = int(float(numbers[0]))
-    minValue = int(float(numbers[0]))
-    for x in range(len(split)):
-        if int(float(split[x])) > maxValue:
-            maxValue = int(float(split[x]))
-        if int(float(split[x])) < minValue:
-            minValue = int(float(split[x]))
-    print(str(maxValue) + " " + str(minValue))
-    return str(maxValue) + " " + str(minValue)
+    numbers = numbers.split(" ")
+    numbers = sorted(numbers, key=int)
+    print(numbers[-1] + " " + numbers[0])
+    return numbers[-1] + " " + numbers[0]
 
 
 high_and_low("8 3 -5 42 -1 0 0 -9 4 7 4 -4")
