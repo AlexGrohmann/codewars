c4d0e5d418ce23f3ad5cbd34310a2fc8cee7a905
AlexGrohmann <alex.grohmann@googlemail.com>
2024-02-22 20:30:26 +0100

    Descending order python fancy solution

--- Descending-order/python.py
+++ Descending-order/python.py
@@ -4,3 +4,7 @@ def descending_order(num):
         result.append(int(d))
     result.sort(reverse=True)
     return int("".join(str(i) for i in result))
+
+
+def descending_order_fancy(num):
+    return int("".join(sorted(str(num), reverse=True)))
