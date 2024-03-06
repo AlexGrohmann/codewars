6609e0ea63cad4af31dd265d1c37fd4fcaf6182c
AlexGrohmann <alex.grohmann@googlemail.com>
2024-03-06 21:04:42 +0100

    add better solution

--- Reversed-strings/go.go
+++ Reversed-strings/go.go
@@ -1,11 +1,27 @@
 package main
 
+import "fmt"
+
+func main() {
+	Solution("hallo")
+	SolutionBetterSolution("hallo")
+}
+
 func Solution(word string) string {
 	runes := []rune(word)
 	size := len(runes)
 	for i, j := 0, size-1; i < size>>1; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
+	fmt.Println(string(runes))
 	return string(runes)
 
+}
+
+func SolutionBetterSolution(word string) (reversed string) {
+	for _, char := range word {
+		reversed = string(char) + reversed
+	}
+	fmt.Printf(reversed)
+	return
 }
