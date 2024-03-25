71e038320533bc5194274c5cf80969daf35aea22
AlexGrohmann <alex.grohmann@googlemail.com>
2024-03-25 20:39:06 +0100

    replace with alphabet index python

---
+++ replace-with-alphabet-index/python.py
@@ -0,0 +1,34 @@
+def alphabet_position(text):
+    alphabet = [
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q",
+        "r",
+        "s",
+        "t",
+        "u",
+        "v",
+        "w",
+        "x",
+        "y",
+        "z",
+    ]
+    result = ""
+    for char in text:
+        if char.isalnum():
+            result = result + " " + str(alphabet.index(char.lower()) + 1)
+    return result.strip()
