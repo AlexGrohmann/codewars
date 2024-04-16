b853a9706fa0a0ee346883ef4e3f6d689e2d2ae6
AlexGrohmann <alex.grohmann@googlemail.com>
2024-04-16 22:21:05 +0200

    number of people in the bus fancy

--- Number-of-people-in-the-bus/python.py
+++ Number-of-people-in-the-bus/python.py
@@ -3,3 +3,7 @@ def number(bus_stops):
     for stop in bus_stops:
         psg = psg + stop[0] - stop[1]
     return psg
+
+
+def number_fancy(bus_stops):
+    return sum([stop[0] - stop[1] for stop in bus_stops])
