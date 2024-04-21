a2afab01c9b9d3a5fc498a6b39ccbbd39bb96f6e
AlexGrohmann <alex.grohmann@googlemail.com>
2024-04-21 16:07:47 +0200

    is n divisible by x and y different if statement

--- Is-n-divisible-by-x-and-y/shell.sh
+++ Is-n-divisible-by-x-and-y/shell.sh
@@ -6,3 +6,15 @@ if (($1 % $2 == 0 && $1 % $3 == 0)); then
     fi
 
 sleep 5
+
+# different solution to learn more about if statements in bash
+n=$1
+x=$2
+y=$3
+a=$((n%x))
+b=$((n%y))
+if [ "$a" == "0" ] && [ "$b" == "0" ]; then
+  echo true
+else 
+  echo false
+fi
