e88d0be6a2f6f0afa48610fe61e379618d8fb0e9
AlexGrohmann <alex.grohmann@googlemail.com>
2024-04-21 15:36:53 +0200

    sqaure sum python fancy

--- Square-sum/python.py
+++ Square-sum/python.py
@@ -1,5 +1,9 @@
 def square_sum(numbers):
     result = 0
     for x in numbers:
-        result = result + x ** 2
+        result = result + x**2
     return result
+
+
+def square_sum_fancy(numbers):
+    return sum(x**2 for x in numbers)
